2dbd6e16d386ac5c043078beb5ecb7f9d3bb0aa0
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-29 01:40:40 +0530

    ADDED : User as entity to store first name and last name

--- src/main/java/com/bridgelabz/myfirstspringapp/model/User.java
+++ src/main/java/com/bridgelabz/myfirstspringapp/model/User.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.myfirstspringapp.model;
 
 public class User {
+    public String firstName;
+    public String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
