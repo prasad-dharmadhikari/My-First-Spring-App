fd7e147821c95ea87ea3ea79b128b9870b764bf7
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-29 00:47:47 +0530

    ADDED : Request mapping with query parameter

--- src/main/java/com/bridgelabz/myfirstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstspringapp/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.myfirstspringapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloWorldController {
     @RequestMapping(value = {"/", "", "/home"})
     public String sayHello() {
         return "Hello World";
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHelloForQueryParameter(@RequestParam(value = "name") String name) {
+        return "Hello " + name;
     }
 }
